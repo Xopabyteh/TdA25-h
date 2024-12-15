9b30a87600fa05e1e10fc54ea22f576e1761e8f3
Xopabyteh <xopabyteh1337@gmail.com>
2024-12-15 13:36:11 +0100

    Delete endpoint

---
+++ src/h.Server/Features/Games/DeleteGame.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using h.Server.Infrastructure;
+using h.Server.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace h.Server.Features.Games;
+
+public static class DeleteGame
+{
+    public class Endpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/api/games/{id}", async (
+                [FromRoute] Guid id,
+                [FromServices] AppDbContext db) =>
+            {
+                var game = await db.GamesDbSet.FindAsync(id);
+
+                if (game is null)
+                    return ErrorResults.NotFound();
+
+                db.GamesDbSet.Remove(game);
+                await db.SaveChangesAsync();
+
+                return Results.StatusCode(204);
+            });
+        }
+    }
+}
