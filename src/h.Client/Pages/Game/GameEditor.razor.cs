b3cf3ee3f7cf6c525a31f9eaba85eed5dff44895
Xopabyteh <xopabyteh1337@gmail.com>
2025-01-12 18:16:30 +0100

    Game editor basic editing

--- src/h.Client/Pages/Game/GameEditor.razor.cs
+++ src/h.Client/Pages/Game/GameEditor.razor.cs
@@ -1,9 +1,100 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Runtime.InteropServices;
 
 namespace h.Client.Pages.Game;
 
-public partial class GameEditor
+/// <summary>
+/// Handles new game creatn and editing of existing games.
+/// </summary>
+public partial class GameEditor : IAsyncDisposable
 {
-    [Parameter]
-    public Guid? GameId { get; set; }
+    [Parameter] public Guid? GameId { get; set; }
+
+
+    [Inject] protected IJSRuntime _js { get; set; } = null!;
+    private IJSObjectReference? jsModule;
+    private CancellationTokenSource disposeCts = new();
+
+    private ElementReference gameFieldRef; 
+
+    public async Task HandleSelectX()
+    {
+        if(jsModule is null)
+            return;
+
+        await jsModule.InvokeVoidAsync("selectXPencil", disposeCts.Token);
+    }
+
+    public async Task HandleSelectO()
+    {
+        if (jsModule is null)
+            return;
+
+        await jsModule.InvokeVoidAsync("selectOPencil", disposeCts.Token);
+    }
+
+    public async Task HandleSelectEraser()
+    {
+        if (jsModule is null)
+            return;
+
+        await jsModule.InvokeVoidAsync("selectEraser", disposeCts.Token);
+    }
+
+    public async Task HandleClearCanvas()
+    {
+        if (jsModule is null)
+            return;
+
+        await jsModule.InvokeVoidAsync("clearCanvas", disposeCts.Token);
+    }
+
+    public async Task HandleHistoryBack()
+    {
+        if (jsModule is null)
+            return;
+
+        await jsModule.InvokeVoidAsync("historyBack", disposeCts.Token);
+    }
+
+    public async Task HandleHistoryForth()
+    {
+        if (jsModule is null)
+            return;
+
+        await jsModule.InvokeVoidAsync("historyForth", disposeCts.Token);
+    }
+
+    public async Task HandleSaveGame()
+    {
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if(!firstRender)
+            return;
+
+        if(RuntimeInformation.ProcessArchitecture != Architecture.Wasm)
+            return; // Prerendering should not load js
+
+
+        jsModule = await _js.InvokeAsync<IJSObjectReference>(
+            "import",
+            disposeCts.Token, "./Pages/Game/GameEditor.razor.js");
+    
+        await jsModule.InvokeVoidAsync(
+            "initializeEditor",
+            disposeCts.Token,
+            gameFieldRef);
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        disposeCts.Cancel();
+        disposeCts.Dispose();
+        
+        return jsModule?.DisposeAsync()
+            ?? ValueTask.CompletedTask;
+    }
 }
---
+++ src/h.Client/Pages/Game/GameEditor.razor.js
@@ -0,0 +1,205 @@
+﻿const X_IMG_PATH = '/img/X/X_cervene.svg';
+const O_IMG_PATH = '/img/O/O_modre.svg';
+
+// 'X' or 'O' or '' for eraser
+const PENCIL_X = 'X';
+const PENCIL_O = 'O';
+const PENCIL_ERASER = '';
+
+let selectedPencil = null;
+const gameField = []; // [y][x], y - row, x - column
+let gameFieldElementRef;
+
+const editHistory = [];
+let historyI = 0;
+
+export const initializeEditor = (
+    _gameFieldElementRef = new Element(),
+    fieldWidth = 15,
+    fieldHeight = 15
+) => {
+    gameFieldElementRef = _gameFieldElementRef;
+
+    // Prepare field
+    for (let y = 0; y < fieldHeight; y++) {
+        gameField.push([]);
+        for (let x = 0; x < fieldWidth; x++) {
+            gameField[y].push('');
+        }
+    }
+
+    appendCurrentStateToEditHistory();
+
+    // Add onclick event to each cell
+    const cells = gameFieldElementRef.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('click', () => handleCellClick(cell));
+    });
+}
+
+const handleCellClick = (
+    cell = new Element()
+) => {
+    if (selectedPencil === null)
+        return; // No pencil selected
+
+    const x = cell.getAttribute('data-x');
+    const y = cell.getAttribute('data-y');
+
+    // Set field value
+    const shouldAppendToHistory = gameField[y][x] !== selectedPencil;
+    gameField[y][x] = selectedPencil;
+
+    // Add/remove image
+    let img = cell.querySelector('img');
+
+    if (selectedPencil === PENCIL_ERASER) {
+        // Eraser
+        if(img) {
+            cell.removeChild(img);
+            if (shouldAppendToHistory) {
+                appendCurrentStateToEditHistory();
+            }
+            return;
+        }
+    }
+
+    // -> Draw
+    const newImgSrc = selectedPencil === PENCIL_X
+        ? X_IMG_PATH
+        : O_IMG_PATH;
+
+    // Append to history if value changed
+    if (shouldAppendToHistory) {
+        appendCurrentStateToEditHistory();
+    }
+
+    if (img) {
+        // Only update src
+        img.src = newImgSrc;
+        return;
+    }
+
+    // -> Create new img element
+    img = document.createElement('img');
+    img.src = newImgSrc;
+    cell.appendChild(img);
+}
+
+export const selectXPencil = () => {
+    selectedPencil = PENCIL_X;
+}
+ 
+export const selectOPencil = () => {
+    selectedPencil = PENCIL_O;
+}
+
+export const selectEraser = () => {
+    selectedPencil = PENCIL_ERASER;
+}
+
+export const clearCanvas = () => {
+    const isClearAlready = gameField.every(row => row.every(cell => cell === ''));
+    if(isClearAlready)
+        return;
+
+    // Clear game field
+    for (let y = 0; y < gameField.length; y++) {
+        for (let x = 0; x < gameField[y].length; x++) {
+            gameField[y][x] = '';
+        }
+    }
+
+    // Remove all cells
+    const cells = gameFieldElementRef.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        const img = cell.querySelector('img');
+        if (img) {
+            cell.removeChild(img);
+        }
+    });
+
+    appendCurrentStateToEditHistory();
+}
+
+const loadField = (
+    newGameField, // [y][x], y - row, x - column
+) => {
+    if (gameFieldElementRef == null)
+        return; // lets hope this wont happen lol
+
+    // Set game field values
+    for (let y = 0; y < gameField.length; y++) {
+        for (let x = 0; x < gameField[y].length; x++) {
+            gameField[y][x] = newGameField[y][x];
+        }
+    }
+
+    const cells = gameFieldElementRef.querySelectorAll('.cell');
+
+    // update images
+    cells.forEach(cell => {
+        const x = cell.getAttribute('data-x');
+        const y = cell.getAttribute('data-y');
+
+        const pencil = gameField[y][x];
+        let img = cell.querySelector('img');
+
+        if (pencil === PENCIL_ERASER) {
+            // Try remove img
+            if (img) {
+                cell.removeChild(img);
+            }
+
+            return;
+        }
+
+        const imgSrc = pencil === PENCIL_X
+            ? X_IMG_PATH
+            : O_IMG_PATH;
+
+        // Draw X or O
+        if (img == null) {
+            // Create new img element
+            img = document.createElement('img');
+            img.src = imgSrc;
+            cell.appendChild(img);
+            return;
+        }
+
+        // -> Only update src
+        img.src = imgSrc;
+    });
+}
+
+export const historyBack = () => {
+    if(historyI <= 0)
+        return;
+
+    historyI--;
+
+    loadField(editHistory[historyI]);
+}
+
+export const historyForth = () => {
+    if(historyI >= editHistory.length - 1)
+        return;
+
+    historyI++;
+
+    loadField(editHistory[historyI]);
+}
+
+const appendCurrentStateToEditHistory = () => {
+    if(historyI < editHistory.length - 1) {
+        editHistory.splice(historyI + 1);
+    };
+
+    const gameFieldDeepCopy = [];
+    for (let y = 0; y < gameField.length; y++) {
+        gameFieldDeepCopy.push([...gameField[y]]);
+    }
+
+    editHistory.push(gameFieldDeepCopy);
+    historyI = editHistory.length - 1;
+}
