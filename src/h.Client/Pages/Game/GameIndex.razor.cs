e9b09ef3fcb9e5aa9a0e454bd10497dc4aac4934
Klára Futej <90642166+futejo-k@users.noreply.github.com>
2025-01-14 14:56:19 +0100

    game index frontend

--- src/h.Client/Pages/Game/GameIndex.razor.cs
+++ src/h.Client/Pages/Game/GameIndex.razor.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
 
 namespace h.Client.Pages.Game;
 public partial class GameIndex
 {
     [Parameter]
     public Guid? GameId { get; set; }
+
+    private bool xTurn = true;
+    private string imgSrc = "";
+    private string imgAlt = "";
 }
