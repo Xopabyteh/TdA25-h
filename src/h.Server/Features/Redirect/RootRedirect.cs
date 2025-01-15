359ed16752b3c7672deb6cf9493696be4ae1efea
Xopabyteh <xopabyteh1337@gmail.com>
2025-01-15 20:17:24 +0100

    Reroute from root to list [no ci]

---
+++ src/h.Server/Features/Redirect/RootRedirect.cs
@@ -0,0 +1,22 @@
+﻿using Carter;
+using h.Client.Pages;
+
+namespace h.Server.Features.Redirect;
+
+/// <summary>
+/// Temporarily redirects root to game list,
+/// as there is no root page yet...
+/// </summary>
+public static class RootRedirect
+{
+    public class Endpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/", () =>
+            {
+                return Results.Redirect(PageRoutes.Game.GameList);
+            });
+        }
+    }
+}
