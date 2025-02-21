3b03666e043e54bd1f57ec66b81951b18b4edafa
Xopabyteh <xopabyteh1337@gmail.com>
2025-02-21 09:23:16 +0100

    Game started identity providing

--- src/h.Contracts/MultiplayerGames/MultiplayerGameStartedResponse.cs
+++ src/h.Contracts/MultiplayerGames/MultiplayerGameStartedResponse.cs
@@ -1,8 +1,19 @@
 ﻿using h.Primitives.Games;
 
 namespace h.Contracts.MultiplayerGames;
+
+/// <summary>
+/// A notification about a new game start. Each user recieves unique notification
+/// with their <see cref="MySessionId"/>
+/// </summary>
+/// <param name="GameId">The id of the game</param>
+/// <param name="MySessionId">The users multiplayer identity for this game session</param>
+/// <param name="StartingPlayer">Starting player</param>
+/// <param name="Players">Multiplayer identities of all players ingame</param>
+/// <param name="PlayerSymbols">Player symbol mappings</param>
 public readonly record struct MultiplayerGameStartedResponse(
     Guid GameId,
+    Guid MySessionId,
     MultiplayerGameUserIdentityDTO StartingPlayer,
     IReadOnlyCollection<MultiplayerGameUserIdentityDTO> Players,
     KeyValuePair<MultiplayerGameUserIdentityDTO, GameSymbol>[] PlayerSymbols
