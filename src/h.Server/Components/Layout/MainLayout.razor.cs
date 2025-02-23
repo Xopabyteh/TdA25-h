3378e745439242444c3eac55b763fbd1e629cf0a
Xopabyteh <xopabyteh1337@gmail.com>
2025-02-23 20:08:34 +0100

    Top menu show user profile with elo SSR

--- src/h.Server/Components/Layout/MainLayout.razor.cs
+++ src/h.Server/Components/Layout/MainLayout.razor.cs
@@ -1,22 +1,43 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components;
-using System.Security.Claims;
+﻿using h.Server.Entities.Users;
+using h.Server.Infrastructure.Auth;
+using h.Server.Infrastructure.Database;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
 
 namespace h.Server.Components.Layout;
-
 public partial class MainLayout
 {
-    [CascadingParameter]
-    public Task<AuthenticationState>? authenticationState { get; set; }
+    private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
+    protected User? currentUser;
+    protected bool isGuest;
 
-    private ClaimsPrincipal? user;
-
-    override protected async Task OnInitializedAsync()
+    public MainLayout(IDbContextFactory<AppDbContext> dbContextFactory, AuthenticationStateProvider authenticationStateProvider)
     {
-        if (authenticationState is null)
+        _dbContextFactory = dbContextFactory;
+        _authenticationStateProvider = authenticationStateProvider;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (RuntimeInformation.ProcessArchitecture == Architecture.Wasm)
             return;
 
-        var authState = await authenticationState;
-        user = authState.User;
+        var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+        if(authState.User is not {Identity: { IsAuthenticated: true } })
+            return;
+
+        var guestId = authState.User.GetGuestId();
+        if(guestId is not null)
+        {
+            isGuest = true;
+            return;
+        }
+
+        var userId = authState.User.GetUserId();
+
+        await using var db = _dbContextFactory.CreateDbContext();
+        currentUser = await db.UsersDbSet.FirstAsync(u => u.Uuid == userId);
     }
 }
