17685f8c932a03fddb2110ecff0761e89cdbb292
Xopabyteh <xopabyteh1337@gmail.com>
2025-02-27 21:19:46 +0100

    game archive and nav menu user link fix

--- src/h.Server/Components/Pages/RankedGameArchive.razor.cs
+++ src/h.Server/Components/Pages/RankedGameArchive.razor.cs
@@ -1,11 +1,43 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using h.Server.Entities.MultiplayerGames;
+using h.Server.Infrastructure.Database;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace h.Client.Pages.User;
+namespace h.Server.Components.Pages;
 
-public partial class GameArchive
+public partial class RankedGameArchive
 {
-    [Parameter] [FromRoute(Name = "gameId")] public int? gameId { get; set; }
-    
+    [Parameter] [FromRoute(Name = "gameId")] public int? GameId { get; set; }
 
+    private FinishedRankedGame? game;
+    private string player1Name;
+    private string player2Name;
+
+    private readonly IDbContextFactory<AppDbContext> _dbContextFactory;
+
+    public RankedGameArchive(IDbContextFactory<AppDbContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (GameId is null)
+        {
+            return;
+        }
+
+        await using var db = _dbContextFactory.CreateDbContext();
+        game = await db.Set<FinishedRankedGame>()
+            .Include(g => g.UserToFinishedRankedGames)
+            .ThenInclude(u => u.User)
+            .FirstOrDefaultAsync(g => g.Id == GameId);
+
+        if (game is null)
+            return;
+
+        player1Name = game.UserToFinishedRankedGames.First(u => u.UserId == game.Player1Id)!.User!.Username;
+        player2Name = game.UserToFinishedRankedGames.First(u => u.UserId == game.Player2Id)!.User!.Username;
+    }
 }
